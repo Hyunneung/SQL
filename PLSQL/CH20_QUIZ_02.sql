f2bec8bd64a8df3f82d2b878f8a5cad5539ad5ab
Hyunneung <neong_@naver.com>
2022-05-29 23:31:28 +0900

    구구단 5단

---
+++ PLSQL/CH20_QUIZ_02.sql
@@ -0,0 +1,20 @@
+--구구단 5단
+SET SERVEROUTPUT ON
+
+DECLARE
+  DAN NUMBER := 5;
+  I NUMBER := 1;
+
+BEGIN
+  LOOP
+    DBMS_OUTPUT.PUT_LINE( 
+	DAN || ' * ' || I || ' = ' || (DAN*I) );
+	
+	I := I+1;
+	IF I >9 THEN
+	EXIT;
+	END IF;
+  END LOOP;
+
+END;
+/		
